cd458d7be3df8ae5607cbfc0a2eb17c6e44cd9f0
M-Aloysie <a.murekatet@alustudent.com>
2023-06-20 21:45:16 +0000

    Tasks Completed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ import re
 
 # Set the username and host for SSH connection to the server
 env.user = 'ubuntu'
-env.hosts = ['3.80.190.160', '3.85.173.231']
+env.hosts = ['54.242.207.186', '54.167.112.194']
 env.key_filename = '~/.ssh/id_rsa'
 
 
